b9e3412fb6011a243c4b0ea8e7f83949b8c4d1b2
tudao@5nhatnhat.com <nevarda9416>
2021-12-31 13:24:16 +0700

    - [Dotnet Core]: Controller in ASP.NET Core MVC Application 1. What is a controller? 2. Role of controller 3. How to add controller in ASP.NET Core Application? 4. What are Action Methods? 5. How to Pass Parameters in Action Methods? 7. When should we create a new controller? 8. How many controllers can we have in a single application?

---
+++ MyFirstCoreWebApp/Controllers/StudentController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstCoreWebApp.Controllers
+{
+    public class StudentController
+    {
+        public string GetStudentsByName(string name)
+        {
+            return $"Return All Students with Name: {name}";
+        }
+    }
+}
