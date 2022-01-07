f0385dba0647577df087de183f292bccad764479
tudao@5nhatnhat.com <nevarda9416>
2022-01-07 12:00:38 +0700

    - [Dotnet Core]: Install twitter-bootstrap and using in ASP.NET Core MVC Application

--- MyFirstCoreWebApp/Controllers/StudentController.cs
+++ MyFirstCoreWebApp/Controllers/StudentController.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFirstCoreWebApp.Models;
+using MyFirstCoreWebApp.Repositories;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MyFirstCoreWebApp.Controllers
 {
-    public class StudentController
+    public class StudentController : Controller
     {
+        public ViewResult Index()
+        {
+            List<Student> listStudents = new List<Student>()
+            {
+                new Student() {StudentId = 101, Name="James",Branch="CSE", Section="A", Gender="Male"},
+                new Student() {StudentId = 102, Name="Smith",Branch="ETC", Section="B", Gender="Male"},
+                new Student() {StudentId = 103, Name="David",Branch="CSE", Section="A", Gender="Male"},
+                new Student() {StudentId = 104, Name="Sara",Branch="CSE", Section="A", Gender="Female"},
+                new Student() {StudentId = 105, Name="Pam",Branch="ETC", Section="B", Gender="Female"},
+            };
+            return View(listStudents);
+        }
+        public ViewResult Detail(int Id)
+        {
+            TestStudentRepository repository = new TestStudentRepository();
+            Student studentDetail = repository.GetStudentById(Id); 
+            return View(studentDetail);
+        }
+
         public string GetStudentsByName(string name)
         {
             return $"Return All Students with Name: {name}";
