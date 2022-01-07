75688bf88aa020db59d874387a515431795209f6
tudao@5nhatnhat.com <nevarda9416>
2022-01-07 08:51:32 +0700

    - [Dotnet Core]: Attribute routing using tokens 1. Understanding tokens in attribute routing 2. Token example in attribute routing 3. Advantages of using tokens in attribute routing 4. Do we need to write the action token on each action method? 5. Attribute routing vs conventional routing in ASP.NET Core

--- MyFirstCoreWebApp/Controllers/HomeController.cs
+++ MyFirstCoreWebApp/Controllers/HomeController.cs
@@ -4,11 +4,9 @@ using MyFirstCoreWebApp.ViewModels;
 
 namespace MyFirstCoreWebApp.Controllers
 {
+    [Route("[controller]/[action]")]
     public class HomeController : Controller
     {
-        [Route("/")]
-        [Route("Home")]
-        [Route("Home/Index")]
         public ViewResult Index()
         {
             return View("Test");
