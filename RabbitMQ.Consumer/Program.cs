1288e9d03d05e235c61076c517033c0e2ed06288
tudao@5nhatnhat.com <nevarda9416>
2022-02-10 12:11:58 +0700

    - [Dotnet Core]: Using RabbitMQ cmd: dotnet add package RabbitMQ.Client cmd: dotnet add package Newtonsoft.Json RabbitMQ is an open-source message broker. And it is probably one of the most widely used message brokers out there RabbitMQ is extremely lightweight and very easy to deploy. It supports multiple protocols. It is highly available and scalable. And finally, it supports multiple operating systems including Windows, Linux and MacOS - What is a message broker? You can think of a message broker like a post office. Its main responsibility is to broker messages between publishers and subscribers Once a message is received by a message broker from a producer, it routes the message to a subscriber. The message broker pattern is one of the most useful patterns when it comes to decoupling microservices (tách các microservice) (*) The message broker consists of three main components: Producers, Consumers and Queues - What is a Producer? A producer is an application responsible for sending message - What is a Consumer? A consumer is an application listening for messages - What is Queue? A queue is a storage where messages are stored by the broker (broker lưu trữ các message vào queue chờ các consumer đến lấy và xử lý/đọc) - Protocols Supported by RabbitMQ RabbitMQ supports multiple protocols: + AMQP 0-9-1: is a binary messaging protocol specification. This is the core protocol specification implemented in RabbitMQ. All other protocol support in RabbitMQ is through plugins Other protocols supported are: + STOMP - A text-based message protocol + MQTT - Binary protocol focusing mainly on Publish/Subscribe scenarios + AMQP 1.0 + HTTP and WebSocket

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,20 @@
+﻿using RabbitMQ.Client;
+using System;
+
+namespace RabbitMQ.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello RabbitMQ!");
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            QueueConsumer.Consume(channel);
+        }
+    }
+}
