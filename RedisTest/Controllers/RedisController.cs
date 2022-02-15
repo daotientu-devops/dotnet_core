d7cadb3409628349b2769dccdec53d30a007f993
tudao@5nhatnhat.com <nevarda9416>
2022-02-15 14:42:34 +0700

    - [Dotnet Core]: Using Redis cmd: dotnet add package Microsoft.Extensions.Caching.StackExchangeRedis

---
+++ RedisTest/Controllers/RedisController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace RedisTest.Controllers
+{
+    [Route("api/[controller]")]
+    public class RedisController : Controller
+    {
+        private readonly IDistributedCache _distributedCache;
+        public RedisController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+        [HttpGet]
+        public string Get()
+        {
+            var cacheKey = "dotnet_core_test_redis";
+            var currentTime = DateTime.Now.ToString();
+            var cachedTime = _distributedCache.GetString(cacheKey);
+            if (string.IsNullOrEmpty(cachedTime))
+            {
+                // Cache expire trong 5s
+                var options = new DistributedCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromSeconds(60));
+                // Nạp lại giá trị mới cho cache
+                _distributedCache.SetString(cacheKey, currentTime, options);
+                cachedTime = _distributedCache.GetString(cacheKey);
+            }
+            var result = $"Current Time: {currentTime} \nCached Time: {cachedTime}";
+            return result;
+        }
+    }
+}
