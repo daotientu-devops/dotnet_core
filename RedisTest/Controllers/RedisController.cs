c5fc9f95e7d1b8a47ecdaf94b293fb974d7d07fa
tudao@5nhatnhat.com <nevarda9416>
2022-02-16 09:02:26 +0700

    - [Dotnet Core]: Using Elasticsearch

--- RedisTest/Controllers/RedisController.cs
+++ RedisTest/Controllers/RedisController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using Microsoft.Extensions.Caching.Distributed;
+using Nest;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace RedisTest.Controllers
 {
@@ -13,7 +17,7 @@ namespace RedisTest.Controllers
             _distributedCache = distributedCache;
         }
         [HttpGet]
-        public string Get()
+        public async Task<string> GetAsync()
         {
             var cacheKey = "dotnet_core_test_redis";
             var currentTime = DateTime.Now.ToString();
@@ -27,7 +31,56 @@ namespace RedisTest.Controllers
                 cachedTime = _distributedCache.GetString(cacheKey);
             }
             var result = $"Current Time: {currentTime} \nCached Time: {cachedTime}";
+
+            // Để nhờ code Elasticsearch Test
+            result += "\nElasticsearch ";
+            var connString = "http://localhost:9200";
+            var settings = new ConnectionSettings(new Uri(connString))
+                .DefaultMappingFor<SearchProductModel>(i => i.IndexName("product"))
+                .PrettyJson();
+            var client = new ElasticClient(settings);
+            var products = new List<SearchProductModel>()
+            {
+                new SearchProductModel()
+                {
+                    Id = 1,
+                    Name = "Product 1"
+                },
+                new SearchProductModel()
+                {
+                    Id = 2,
+                    Name = "Product 2"
+                },
+                new SearchProductModel()
+                {
+                    Id = 0,
+                    Name = "Product 0"
+                },
+            };
+            for (var index = 0; index < products.Count; index++)
+            {
+                var response = await client.IndexDocumentAsync(products[index]);
+                result += "\n" + response.IsValid;
+            }
+            var resultElasticsearch = await client.SearchAsync<SearchProductModel>(s => s.Query(q => q.MatchAll()));
+            var request = new SearchRequest
+            {
+                From = 0,
+                Size = 10,
+                Query = new TermQuery { Field = "Id", Value = "1"} ||
+                        new MatchQuery { Field = "Name", Query = "Product"}
+            };
+            result += "\n" + resultElasticsearch.Hits.Count;
+            var option = new JsonSerializerOptions();
+            option.WriteIndented = true;
+            result += "\n" + JsonSerializer.Serialize(resultElasticsearch, option);
             return result;
         }
+    }
+
+    internal class SearchProductModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
